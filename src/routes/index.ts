0b7ea457dd8007dff2a99f5a210ea64a191ccb1b
joaobenthin <joaopaulobenthin@hotmail.com>
2020-05-13 18:28:55 -0300

    Prettier

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = Router();
 
 routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello GoStack' })
-})
+  return response.json({ message: 'Hello GoStack' });
+});
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from 'express';
 import routes from './routes';
 
 const app = express();
@@ -6,5 +6,5 @@ const app = express();
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!')
-})
+  console.log('🚀 Server started on port 3333!');
+});
