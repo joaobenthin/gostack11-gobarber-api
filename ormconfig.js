1672c9d5cc7e021af24a55f3b6f12d84c87314f5
joaobenthin <joaopaulobenthin@hotmail.com>
2020-07-10 10:25:49 -0300

    Criando tabela de agendamentos

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,10 @@
-import path from 'path';
+const path = require('path');
 
 module.exports = {
-  type: 'sqlite3',
+  type: 'sqlite',
   database: path.resolve(__dirname, 'database.sqlite'),
+  migrations: ['./src/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/database/migrations',
+  },
 };
